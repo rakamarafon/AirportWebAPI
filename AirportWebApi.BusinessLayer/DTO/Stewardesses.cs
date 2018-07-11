b9eb95fe3d96c835934828b2ea552deaaecc26d9
Maksym Akulov <maksakulov@hotmail.com>
2018-07-11 12:15:54 +0300

    added class Stewardesses  to model(fixes #6)

---
+++ AirportWebApi.BusinessLayer/DTO/Stewardesses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportWebAPI.BusinessLayer.DTO
+{
+    public class Stewardesses
+    {
+        private string _firstName;
+        private string _secondName;
+        private string _birthDate;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { if (value != null) _firstName = value; }
+        }
+
+        public string SecondName
+        {
+            get { return _secondName; }
+            set { if (value != null) _secondName = value; }
+        }
+
+        public string BirthDate
+        {
+            get { return _birthDate; }
+            set { if (value != null) _birthDate = value; }
+        }
+    }
+}

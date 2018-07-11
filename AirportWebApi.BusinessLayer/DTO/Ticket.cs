96e9c2cd26463218f818d60f9770274f59154f9a
Maksym Akulov <maksakulov@hotmail.com>
2018-07-11 12:27:13 +0300

    added class Ticket(fixes #8)

---
+++ AirportWebApi.BusinessLayer/DTO/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportWebAPI.BusinessLayer.DTO
+{
+    public class Ticket
+    {
+        private double _price;
+        private string _flightNumber;
+
+        public double Price
+        {
+            get { return _price; }
+            set { if (value > 0) _price = value; }
+        }
+
+        public string FlightNumber
+        {
+            get { return _flightNumber; }
+            set { if (value != null) _flightNumber = value; }
+        }
+    }
+}

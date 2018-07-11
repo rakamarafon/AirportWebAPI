00d7b0a98575b4c3e79bbbbc34da5b237253e451
Maksym Akulov <maksakulov@hotmail.com>
2018-07-11 11:13:09 +0300

    added class Aircraft to model(fixes #3)

---
+++ AirportWebApi.BusinessLayer/DTO/Aircraft.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportWebAPI.BusinessLayer.DTO
+{
+    public class Aircraft
+    {
+        private string _name;
+        private AirType _airType;
+        private string _releaseDate;
+        private string _lifeTime;
+
+        public string Name
+        {
+            get { return _name; }
+            set { if (value != null) _name = value; }
+        }
+
+        public AirType AirType
+        {
+            get { return _airType; }
+            set { if (value != null) _airType = value; }
+        }
+
+        public string ReleaseDate
+        {
+            get { return _releaseDate; }
+            set { if (value != null) _releaseDate = value; }
+        }
+
+        public string LifeTime
+        {
+            get { return _lifeTime; }
+            set { if (value != null) _lifeTime = value; }
+        }
+    }
+}

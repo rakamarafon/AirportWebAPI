85e2a6fd02531affaa28ef09f400fdd4560f4fe5
Maksym Akulov <maksakulov@hotmail.com>
2018-07-11 12:23:38 +0300

    added class Crew(fixes #4)

---
+++ AirportWebApi.BusinessLayer/DTO/Crew.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportWebAPI.BusinessLayer.DTO
+{
+    public class Crew
+    {
+        private Pilot _pilot;
+        private List<Stewardesses> _stewardesses;
+
+        public Pilot Pilot
+        {
+            get { return _pilot; }
+            set { if (value != null) _pilot = value; }
+        }
+
+        public List<Stewardesses> Stewardesses
+        {
+            get { return _stewardesses; }
+            set { if (value.Count >= 1) _stewardesses = value; }
+        }
+    }
+}

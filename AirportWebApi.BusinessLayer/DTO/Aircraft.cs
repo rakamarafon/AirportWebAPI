4f02ed9054d00e7795860ae64831c0dd4f90e169
Maksym Akulov <maksakulov@hotmail.com>
2018-07-12 14:25:14 +0300

    Add service for Aircraft (fixes #29)

--- AirportWebApi.BusinessLayer/DTO/Aircraft.cs
+++ AirportWebApi.BusinessLayer/DTO/Aircraft.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AirportWebAPI.BusinessLayer.DTO
 {
-    public class Aircraft
+    public class Aircraft : BaseObject
     {
         private string _name;
         private int _airType;

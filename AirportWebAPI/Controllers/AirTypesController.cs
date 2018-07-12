db8f2d18bc2bed5d00e135328f80f3db653aa919
Maksym Akulov <maksakulov@hotmail.com>
2018-07-12 13:53:58 +0300

    Added controller for AirType (fixes #25)

--- AirportWebAPI/Controllers/AirTypesController.cs
+++ AirportWebAPI/Controllers/AirTypesController.cs
@@ -1,37 +1,42 @@
-﻿using AirportWebAPI.BusinessLayer.DTO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirportWebAPI.BusinessLayer.DTO;
 using AirportWebAPI.BusinessLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AirportWebAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class CrewController : Controller
+    public class AirTypesController : Controller
     {
-        private readonly IService<Crew> service;
+        private readonly IService<AirType> service;
 
-        public CrewController(IService<Crew> service)
+        public AirTypesController(IService<AirType> service)
         {
             this.service = service;
         }
-        // GET: api/Crew
+        // GET: api/AirTypes
         [HttpGet]
         public IActionResult Get()
         {
             return Ok(service.GetAll());
         }
 
-        // GET: api/Crew/5
+        // GET: api/AirTypes/5
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
             var temp = service.GetById(id);
-            if (temp == null) return NotFound(string.Format("Crew with this ID {0} not found", id));
+            if (temp == null) return NotFound(string.Format("AirType with this ID {0} not found", id));
             return Ok(temp);
         }
         
-        // POST: api/Crew
+        // POST: api/AirTypes
         [HttpPost]
-        public IActionResult Post([FromBody]Crew value)
+        public IActionResult Post([FromBody]AirType value)
         {
             var temp = service.AddEntry(value);
             if (temp == null)
@@ -44,9 +49,9 @@ namespace AirportWebAPI.Controllers
             }
         }
         
-        // PUT: api/Crew/5
+        // PUT: api/AirTypes/5
         [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody]Crew value)
+        public IActionResult Put(int id, [FromBody]AirType value)
         {
             var temp = service.UpdateEntry(value);
             if (temp == null)
@@ -70,7 +75,7 @@ namespace AirportWebAPI.Controllers
             }
             else
             {
-                return Ok(string.Format("Crew with ID {0} was successfuly deleted", id));
+                return Ok(string.Format("AirType with ID {0} was successfuly deleted", id));
             }
         }
     }
